e6ac83e40ce7445777a3c8fb812124fc480192c4
reza <rezasemyari1991@gmail.com>
2020-07-28 17:49:35 +0430

    Updates

---
+++ precache-manifest.27c38a901cc81896ad596cc051b7f3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a634aff1d56ffdb67c6dae6d8095627",
+    "url": "/maktab_react-router-PhoneBook/index.html"
+  },
+  {
+    "revision": "048043ca3f4e54030833",
+    "url": "/maktab_react-router-PhoneBook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c36d260faae307d25a0",
+    "url": "/maktab_react-router-PhoneBook/static/js/2.4f1eea8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maktab_react-router-PhoneBook/static/js/2.4f1eea8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048043ca3f4e54030833",
+    "url": "/maktab_react-router-PhoneBook/static/js/main.b41d0636.chunk.js"
+  },
+  {
+    "revision": "b96d1c7455cc04bae87a",
+    "url": "/maktab_react-router-PhoneBook/static/js/runtime-main.7f0d1151.js"
+  }
+]);
