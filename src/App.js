7ba3f1cd143fd8f8cd1644ea3839e4136dd563aa
reza <rezasemyari1991@gmail.com>
2020-07-28 20:20:40 +0430

    create home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                        <Link to="/" className="nav-link font-weight-bold text-white">Home</Link>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link to="/contacts" className="text-white">Contacts</Link>
+                                </li>
+                                <li className="nav-item ml-3">
+                                    <Link to="/contacts/add" className="text-white">Add New Contacts</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <Switch>
+                        <Route exact path="/contacts/add">
+                            
+                        </Route>
+                        <Route exact path="/contacts">
+                            
+                        </Route>
+                        <Route path="/">
+                            
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
 import App from './App';
+import '../node_modules/bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 render(<App/>, document.getElementById('root'));
